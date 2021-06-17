2acaa0bfb7430c00e79d243d81caab7acd22c069
ssalanitri <sergiosalanitri@gmail.com>
2021-06-17 09:58:48 -0300

    Updates

---
+++ precache-manifest.3caee83f229ab9b9f5af846362060329.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a0e3bf290cf4b96eec7665e7daed26",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4a043e67e3c2c3dbfa51",
+    "url": "./static/js/2.dfc7b74e.chunk.js"
+  },
+  {
+    "revision": "c44788151704c2d2d589",
+    "url": "./static/js/main.ae29e879.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
